d9ff11d8f4ad806cf8c24180ce0f8aef1e72d706
Cayal Walker <cayalw@gmail.com>
2021-03-11 20:24:22 -0500

    It's not always best to think in black and white, but it's always a good start to just split things down the middle in the simplest possible way.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use nannou::prelude::*;
 
 struct Model {
     _window: window::Id,
+    split: usize
 }
 
 fn main() {
@@ -10,9 +11,33 @@ fn main() {
 
 fn model(app: &App) -> Model {
     let _window = app.new_window().view(view).build().unwrap();
-    return Model { _window };
+    return Model { _window, split: 1 };
 }
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(_app: &App, _model: &Model, _frame: Frame) {}
+fn view(_app: &App, _model: &Model, _frame: Frame) {
+    let draw = _app.draw();
+    draw.background().color(GREY);
+    let win = _app.window_rect();
+    if _model.split == 1 {
+        let rs = win.subdivisions();
+        draw.rect()
+            .xy(rs[0].xy())
+            .wh(rs[0].wh())
+            .color(BLACK);
+        draw.rect()
+            .xy(rs[2].xy())
+            .wh(rs[2].wh())
+            .color(BLACK);
+        draw.rect()
+            .xy(rs[1].xy())
+            .wh(rs[1].wh())
+            .color(WHITE);
+        draw.rect()
+            .xy(rs[3].xy())
+            .wh(rs[3].wh())
+            .color(WHITE);
+    }
+    draw.to_frame(_app, &_frame).unwrap();
+}
