62e5e21976b0a9a188bba4eb002d99a6e8f7a0fc
Cayal Walker <cayalw@gmail.com>
2021-03-12 22:55:11 -0500

    Adds one more subdivision, laying the groundwork for abstracting draw

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ fn model(app: &App) -> Model {
     return Model { _window, split: 1 };
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+    if _model.split == 1 { _model.split = _model.split + 1 }
+}
 
 fn view(_app: &App, _model: &Model, _frame: Frame) {
     let draw = _app.draw();
@@ -26,6 +28,16 @@ fn view(_app: &App, _model: &Model, _frame: Frame) {
         draw.rect().xy(rs[2].xy()).wh(rs[2].wh()).color(BLACK);
         draw.rect().xy(rs[1].xy()).wh(rs[1].wh()).color(WHITE);
         draw.rect().xy(rs[3].xy()).wh(rs[3].wh()).color(WHITE);
+    }
+    if _model.split == 2 {
+        let rs = win.subdivisions();
+        for r in rs.iter() {
+            let r2s = r.subdivisions();
+            draw.rect().xy(r2s[0].xy()).wh(r2s[0].wh()).color(BLACK);
+            draw.rect().xy(r2s[2].xy()).wh(r2s[2].wh()).color(WHITE);
+            draw.rect().xy(r2s[1].xy()).wh(r2s[1].wh()).color(BLACK);
+            draw.rect().xy(r2s[3].xy()).wh(r2s[3].wh()).color(WHITE);
+        }
     }
     draw.to_frame(_app, &_frame).unwrap();
 }
